6d3af4736e8f0edb8638ea82d1d642178fe52064
Azoz9ll <khaldzwz047@gmail.com>
2026-03-30 13:05:53 +0300

    Initial Commit

---
+++ Smart bill/Smart_billApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_billApp.swift
+//  Smart bill
+//
+//  Created by abdulaziz on 30/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Smart_billApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Smart billTests/Smart_billTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_billTests.swift
+//  Smart billTests
+//
+//  Created by abdulaziz on 30/03/2026.
+//
+
+import Testing
+@testable import Smart_bill
+
+struct Smart_billTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
